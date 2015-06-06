60b8fd5a56a72b4e4aa73a75857b3aa6d7bd5cfb
Art4 <art4@wlabs.de>
2015-06-06 13:41:38 +0200

    Unittests

---
+++ tests/unit/ClientTest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Youthweb\Api\Tests;
+
+use Youthweb\Api\Client;
+use InvalidArgumentException;
+
+class ClientTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * @test
+	 */
+	public function testSetUrlReturnsClient()
+	{
+		$client = new Client();
+
+		$this->assertInstanceOf('Youthweb\Api\Client', $client->setUrl('http://test.local'));
+	}
+
+	/**
+	 * @test
+	 */
+	public function testGetUrlReturnsValueFromSetUrl()
+	{
+		$client = (new Client())->setUrl('http://test.local');
+
+		$this->assertSame('http://test.local', $client->getUrl());
+	}
+
+	/**
+	 * @test
+	 */
+	public function testSetHttpClientReturnsClient()
+	{
+		$client = new Client();
+
+		$stub = $this->getMock('Youthweb\Api\HttpClientInterface');
+
+		$this->assertInstanceOf('Youthweb\Api\Client', $client->setHttpClient($stub));
+	}
+
+	/**
+	 * @test
+	 * @dataProvider getResoursesClassesProvider
+	 */
+	public function testGetApiInstance($resource_name, $class_name)
+	{
+		$client = new Client();
+
+		$this->assertInstanceOf($class_name, $client->getResource($resource_name));
+	}
+
+	public function getResoursesClassesProvider()
+	{
+		return array(
+			array('account', 'Youthweb\Api\Resource\Account'),
+		);
+	}
+}
