05f033f237faee08b4107d7a85957d8adf31e247
Art4 <art4@wlabs.de>
2016-10-17 23:47:07 +0200

    Refactor tests, mock ClientInterface

--- tests/unit/Resource/AuthTest.php
+++ tests/unit/Resource/AuthTest.php
@@ -2,12 +2,7 @@
 
 namespace Youthweb\Api\Tests\Resource;
 
-use Youthweb\Api\Client;
-use Youthweb\Api\HttpClientInterface;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamInterface;
 use Youthweb\Api\Resource\Auth;
-use InvalidArgumentException;
 
 class AuthTest extends \PHPUnit_Framework_TestCase
 {
@@ -16,29 +11,63 @@ class AuthTest extends \PHPUnit_Framework_TestCase
 	 */
 	public function testGetBearerTokenReturnsToken()
 	{
-		$body = $this->getMockBuilder('Psr\Http\Message\StreamInterface')
+		$cache_item = $this->getMockBuilder('Psr\Cache\CacheItemInterface')
 			->getMock();
 
-		$body->expects($this->once())
-			->method('getContents')
-			->willReturn('{"meta":{"token_type":"Bearer","token":"JWT"}}');
+		$cache_item->expects($this->once())
+			->method('isHit')
+			->willReturn(false);
 
-		$response = $this->getMockBuilder('Psr\Http\Message\ResponseInterface')
+		$cache_item->expects($this->once())
+			->method('get')
+			->willReturn('Bearer JWT');
+
+		$cache_pool = $this->getMockBuilder('Psr\Cache\CacheItemPoolInterface')
 			->getMock();
 
-		$response->expects($this->once())
-			->method('getBody')
-			->willReturn($body);
+		$cache_pool->expects($this->once())
+			->method('getItem')
+			->with('cache_item_key')
+			->willReturn($cache_item);
 
-		$http_client = $this->getMockBuilder('Youthweb\Api\HttpClientInterface')
+		$client = $this->getMockBuilder('Youthweb\Api\ClientInterface')
 			->getMock();
 
-		$http_client->expects($this->once())
-			->method('send')
-			->willReturn($response);
+		$client->expects($this->once())
+			->method('buildCacheKey')
+			->willReturn('cache_item_key');
 
-		$client = new Client();
-		$client->setHttpClient($http_client);
+		$client->expects($this->exactly(2))
+			->method('getCacheProvider')
+			->willReturn($cache_pool);
+
+		$client->expects($this->exactly(2))
+			->method('getUserCredential')
+			->will($this->returnValueMap([
+				['username', 'User'],
+				['token_secret', 'secret'],
+			]));
+
+		$document = $this->getMockBuilder('Art4\JsonApiClient\DocumentInterface')
+			->getMock();
+
+		$document->expects($this->exactly(2))
+			->method('has')
+			->will($this->returnValueMap([
+				['meta.token', true],
+				['meta.token_type', true],
+			]));
+
+		$document->expects($this->exactly(2))
+			->method('get')
+			->will($this->returnValueMap([
+				['meta.token', 'JWT'],
+				['meta.token_type', 'Bearer'],
+			]));
+
+		$client->expects($this->once())
+			->method('postUnauthorized')
+			->willReturn($document);
 
 		$auth = new Auth($client);
 
