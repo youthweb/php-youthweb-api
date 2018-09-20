45d912c8620ce0207d9d06f8ca8ae8c6b07ed06f
Art4 <art4@wlabs.de>
2018-09-20 13:37:49 +0200

    Add license note in every file

--- src/Exception/UnauthorizedException.php
+++ src/Exception/UnauthorizedException.php
@@ -1,4 +1,21 @@
 <?php
+/*
+ * PHP Youthweb API is an object-oriented wrapper for PHP of the Youthweb API.
+ * Copyright (C) 2015-2018  Youthweb e.V.  https://youthweb.net
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 namespace Youthweb\Api\Exception;
 
